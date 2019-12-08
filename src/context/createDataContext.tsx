323793dad9e3d506b5d1dffeb1742466259174de
Orlando Arzola <orlando.arzola@externos.bci.cl>
2019-12-08 14:16:18 -0300

    remove commented code and unused imports

--- src/context/createDataContext.tsx
+++ src/context/createDataContext.tsx
@@ -1,23 +1,4 @@
-import React, { useReducer } from 'react';
-import { AppState, PlaylistReducer } from './PlaylistContext';
-
-// export default (reducer: PlaylistReducer, actions, initialState: AppState) => {
-//     const Context = React.createContext<AppState | null>(null);
-
-//     const Provider = ({ children }) => {
-//         const [ state, dispatch ] = useReducer(reducer, initialState);
-
-//         const boundActions = {};
-//         for (let key in actions) {
-//             boundActions[key] = actions[key](dispatch);
-//         }
-
-//         return <Context.Provider value={ state }>{ children }</Context.Provider>
-
-//     }
-
-//     return { Context, Provider }
-// }
+import React from 'react';
 
 export function createCtx<StateType, ActionType>(
     reducer: React.Reducer<StateType, ActionType>,
